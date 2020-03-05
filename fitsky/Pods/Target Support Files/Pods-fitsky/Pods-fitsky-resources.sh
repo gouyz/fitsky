6c17abc8bcf5391e072891f7ef81e99db3b4fd56
gouyz <413977349@qq.com>
2020-03-05 22:35:31 +0800

    111

--- fitsky/Pods/Target Support Files/Pods-fitsky/Pods-fitsky-resources.sh
+++ fitsky/Pods/Target Support Files/Pods-fitsky/Pods-fitsky-resources.sh
@@ -99,22 +99,10 @@ EOM
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_ROOT}/AMap3DMap-NO-IDFA/MAMapKit.framework/AMap.bundle"
   install_resource "${PODS_ROOT}/AlipaySDK-iOS/AlipaySDK.bundle"
-  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RongCloud.bundle"
-  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/en.lproj"
-  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/zh-Hans.lproj"
-  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/Emoji.plist"
-  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RCColor.plist"
-  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RCConfig.plist"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_ROOT}/AMap3DMap-NO-IDFA/MAMapKit.framework/AMap.bundle"
   install_resource "${PODS_ROOT}/AlipaySDK-iOS/AlipaySDK.bundle"
-  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RongCloud.bundle"
-  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/en.lproj"
-  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/zh-Hans.lproj"
-  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/Emoji.plist"
-  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RCColor.plist"
-  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RCConfig.plist"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
