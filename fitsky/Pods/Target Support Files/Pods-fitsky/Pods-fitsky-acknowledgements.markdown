5fb9c46e81cf345142dcd179e61f378fcd377316
gouyz <413977349@qq.com>
2020-03-10 23:40:52 +0800

    111

--- fitsky/Pods/Target Support Files/Pods-fitsky/Pods-fitsky-acknowledgements.markdown
+++ fitsky/Pods/Target Support Files/Pods-fitsky/Pods-fitsky-acknowledgements.markdown
@@ -971,6 +971,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
+## RongCloudIM
+
+Copyright 2014 Rong Cloud
+
 ## SDWebImage
 
 Copyright (c) 2009-2018 Olivier Poitrey rs@dailymotion.com
--- fitsky/Pods/Target Support Files/Pods-fitsky/Pods-fitsky-resources.sh
+++ fitsky/Pods/Target Support Files/Pods-fitsky/Pods-fitsky-resources.sh
@@ -99,10 +99,24 @@ EOM
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "${PODS_ROOT}/AMap3DMap-NO-IDFA/MAMapKit.framework/AMap.bundle"
   install_resource "${PODS_ROOT}/AlipaySDK-iOS/AlipaySDK.bundle"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RongCloud.bundle"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/en.lproj"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/zh-Hans.lproj"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/Emoji.plist"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RCColor.plist"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RCConfig.plist"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RongSticker.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "${PODS_ROOT}/AMap3DMap-NO-IDFA/MAMapKit.framework/AMap.bundle"
   install_resource "${PODS_ROOT}/AlipaySDK-iOS/AlipaySDK.bundle"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RongCloud.bundle"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/en.lproj"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/zh-Hans.lproj"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/Emoji.plist"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RCColor.plist"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RCConfig.plist"
+  install_resource "${PODS_ROOT}/RongCloudIM/RongCloudIM/RongSticker.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
