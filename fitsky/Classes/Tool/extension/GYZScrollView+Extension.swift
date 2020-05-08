9cad712996fdb714d576c1e59b45f7877bf05398
gouyz <413977349@qq.com>
2020-05-08 15:59:17 +0800

    111

--- fitsky/Classes/Tool/extension/GYZScrollView+Extension.swift
+++ fitsky/Classes/Tool/extension/GYZScrollView+Extension.swift
@@ -10,18 +10,18 @@ import UIKit
 
 extension UIScrollView {
 
-    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.next?.touchesBegan(touches, with: event)
-        super.touchesBegan(touches, with: event)
-    }
-    open override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.next?.touchesMoved(touches, with: event)
-        super.touchesMoved(touches, with: event)
-    }
-    open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.next?.touchesEnded(touches, with: event)
-        super.touchesEnded(touches, with: event)
-    }
+//    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        self.next?.touchesBegan(touches, with: event)
+//        super.touchesBegan(touches, with: event)
+//    }
+//    open override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        self.next?.touchesMoved(touches, with: event)
+//        super.touchesMoved(touches, with: event)
+//    }
+//    open override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        self.next?.touchesEnded(touches, with: event)
+//        super.touchesEnded(touches, with: event)
+//    }
 //    open override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
 //        return self
 //    }
